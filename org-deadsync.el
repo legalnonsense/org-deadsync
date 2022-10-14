f82eed309df839d44d9e411e1dc5de54c93862b5
Jeff Filipovits <jrfiliplovits@gmail.com>
2022-10-14 14:47:40 -0400

    stop updating all headings on open

--- org-deadsync.el
+++ org-deadsync.el
@@ -433,12 +433,12 @@ then adjust backward to the previous Friday."
     (define-key org-deadsync-mode-map (kbd "<S-up>") #'org-deadsync-org-shiftup)
     (define-key org-deadsync-mode-map (kbd "<S-down>") #'org-deadsync-org-shiftdown)
     (define-key org-deadsync-mode-map (kbd "<S-left>") #'org-deadsync-org-shiftleft)
-    org-deadsync-mode-map)
-  (if org-deadsync-mode
-      (progn	
-	(org-deadsync-load-all-overlays-and-text-props))
-    (org-deadsync--clear-all)))
+    org-deadsync-mode-map))
+;; (if org-deadsync-mode
+;;     (progn	
+;; 	(org-deadsync-load-all-overlays-and-text-props))
+;;   (org-deadsync--clear-all)))
 
-  (provide 'org-deadsync)
+(provide 'org-deadsync)
 
 ;;; org-deadsync.el ends here
